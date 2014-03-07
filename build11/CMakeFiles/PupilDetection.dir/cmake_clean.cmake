55d326cfc2fc6dd2b6dee4b091d495b37feae474
niannianli <niannian_li@qq.com>
2014-03-07 14:20:01 -0500

    1

---
+++ build11/CMakeFiles/PupilDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PupilDetection.dir/PupilDetection.cpp.o"
+  "PupilDetection.pdb"
+  "PupilDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PupilDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
